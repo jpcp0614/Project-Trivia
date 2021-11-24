35e48fa00457818f7c5942cc7db7a5c9e5a60862
Arthur Teixeira <tuzinf5@gmail.com>
2021-11-24 18:32:03 -0300

    Manter base de thunk comentado

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,15 @@
 export const LOGIN = 'LOGIN';
 
+// export const SUCCESS = 'SUCCESS';
+
+// export const ERROR = 'ERROR';
+
 export const loginAction = (payload) => ({
   type: LOGIN,
   payload,
 });
+
+// export const successActionApi = () => ({
+//   type: SUCCESS,
+//   payload,
+// });
