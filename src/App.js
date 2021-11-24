6a6bef6b2336b66cb2501dbf48ba523d8e3f958c
Arthur Teixeira <tuzinf5@gmail.com>
2021-11-24 18:34:42 -0300

    Cria rota para TriviaScreen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
-import logo from './trivia.png';
+import TriviaScreen from './pages/TriviaScreen';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ TriviaScreen } />
       </Switch>
     </div>
   );
