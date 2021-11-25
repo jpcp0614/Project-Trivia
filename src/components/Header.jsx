43041418f639950bd1dcd47554556b3881e0ab30
jpcp0614 <jpcp0614@gmail.com>
2021-11-25 10:56:10 -0300

    Fetch do gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,36 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      getHash: '',
+    };
+    this.fetchGravatarApi = this.fetchGravatarApi.bind(this);
+  }
+
+  componentDidMount() {
+    const { email } = this.props;
+    this.fetchGravatarApi(email);
+  }
+
+  fetchGravatarApi(email) {
+    const hash = md5(email).toString();
+    this.setState({
+      getHash: `https://www.gravatar.com/avatar/${hash}`,
+    });
+  }
+
   render() {
     const { name } = this.props;
+    const { getHash } = this.state;
     return (
       <header>
         <img
-          src=""
+          src={ getHash }
           alt="Foto de perfil do usuário"
           data-testid="header-profile-picture"
         />
@@ -20,11 +42,13 @@ class Header extends Component {
 }
 
 Header.propTypes = {
+  email: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   name: state.login.name,
+  email: state.login.email,
 });
 
 export default connect(mapStateToProps)(Header);
