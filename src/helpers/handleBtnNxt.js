96cc1f3455710bb8e625e810b2eb38578b6bd1c7
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-11-26 17:21:40 -0300

    Add lógica de ir para feedbacks quando as perguntas acabam

--- src/helpers/handleBtnNxt.js
+++ src/helpers/handleBtnNxt.js
@@ -1,5 +1,10 @@
 export default function handleBtnNxt() {
   const { questionSelector } = this.state;
+  const { history } = this.props;
+  const FOUR = 4;
+  if (questionSelector === FOUR) {
+    history.push('/feedback');
+  }
   this.setState({
     questionSelector: questionSelector + 1,
   });
